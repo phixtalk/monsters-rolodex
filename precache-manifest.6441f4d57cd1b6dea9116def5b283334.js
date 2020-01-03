58d04dfc1ed32fdf8b0f5ba67629508bd3189d4d
Ofoefule Christian <ofoefule.c@gmail.com>
2020-01-03 10:15:57 +0100

    Updates

---
+++ precache-manifest.6441f4d57cd1b6dea9116def5b283334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a8044f9aff913eef7282a1977e7811",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1bbda1f57be635cad5e0",
+    "url": "/monsters-rolodex/static/css/main.4b6207cc.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bbda1f57be635cad5e0",
+    "url": "/monsters-rolodex/static/js/main.b529030f.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
